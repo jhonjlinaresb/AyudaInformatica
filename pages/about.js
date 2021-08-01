7536eb042dfd09a15565e3477407c7b08b1556a1
unknown <jhonjlinaresb@gmail.com>
2021-08-01 18:14:09 +0200

    Create a New Project with NextJS

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+        <>
+    <div>Esto es sobre mi</div>
+    <Link href="/"><a className="backLink">Back</a></Link>
+      </>
+    );
+    
+  }
